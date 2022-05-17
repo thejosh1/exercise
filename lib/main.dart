44995b91ffebbde3601529186d3f23c0f4ff6c3c
thejosh1 <jocalex8@gmail.com>
2022-05-17 14:27:06 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
-import 'package:exercise/pages/drag_animations.dart';
-import 'package:exercise/pages/home_page.dart';
-import 'package:exercise/pages/main_page.dart';
-import 'package:exercise/pages/product_after_page.dart';
-import 'package:exercise/pages/product_before_page.dart';
-import 'package:exercise/pages/tracking_page.dart';
-import 'package:exercise/pages/welcome_page2.dart';
-import 'package:exercise/pages/welcome_page3.dart';
+import 'package:exercise/pages/collection_page.dart';
 
 import 'package:flutter/material.dart';
 
@@ -28,7 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
           extendBody: true,
-          body:DragAnimations()
+          body:CollectionsPage()
       ),
     );
   }
--- lib/pages/collection_page.dart
+++ lib/pages/collection_page.dart
@@ -1,7 +1,8 @@
 import 'package:exercise/widgets/app_large_text.dart';
 import 'package:exercise/widgets/app_text.dart';
-import 'package:exercise/widgets/responsive_circle.dart';
+
 import 'package:flutter/material.dart';
+
 class CollectionsPage extends StatefulWidget {
   const CollectionsPage({Key? key}) : super(key: key);
 
@@ -17,13 +18,13 @@ class _CollectionsPageState extends State<CollectionsPage> {
       body: Column(
         children: [
           Container(
-              margin: const EdgeInsets.only(left: 20, top: 30,),
+              margin: EdgeInsets.only(left: MediaQuery.of(context).size.width/18, top: MediaQuery.of(context).size.height/12.2,),
               child: Row(
                 children: [
                   const Icon(Icons.menu),
                   Container(
-                    margin: const EdgeInsets.only(left: 60, right: 60),
-                    child: AppLargeText(text: "Collections", color: Colors.red, size: 30,),
+                    margin: EdgeInsets.only(left: MediaQuery.of(context).size.width/6, right: MediaQuery.of(context).size.width/6),
+                    child: AppLargeText(text: "Collections", color: Colors.red, size: MediaQuery.of(context).size.width/12,),
                   ),
                   const Icon(Icons.search)
                 ],
@@ -31,42 +32,42 @@ class _CollectionsPageState extends State<CollectionsPage> {
 
           ),
           Container(
-            margin: const EdgeInsets.only(top: 30),
-            height: 300,
-            width: 300,
+            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/24.4),
+            height: MediaQuery.of(context).size.height/2.4,
+            width: MediaQuery.of(context).size.width/1.2,
             child: Center(
               child: Stack(
                 children: [
                   Positioned(
-                    height: 280,
-                    width: 200,
-                    left: 60,
+                    height: MediaQuery.of(context).size.height/2.6,
+                    width: MediaQuery.of(context).size.width/1.8,
+                    left: MediaQuery.of(context).size.width/6,
                     child: Container(
                       decoration: BoxDecoration(
                           color: Colors.grey.withOpacity(0.2),
-                          borderRadius: BorderRadius.circular(20)
+                          borderRadius: BorderRadius.circular(MediaQuery.of(context).size.height/36.65)
                       ),
                     ),
                   ),
                   Positioned(
-                      height: 270,
-                      width: 230,
-                      left: 40,
+                      height: MediaQuery.of(context).size.height/2.7,
+                      width: MediaQuery.of(context).size.height/1.6,
+                      left: MediaQuery.of(context).size.width/9,
                       child: Container(
                         decoration: BoxDecoration(
                             color: Colors.grey.withOpacity(0.5),
-                            borderRadius: BorderRadius.circular(20)
+                            borderRadius: BorderRadius.circular(MediaQuery.of(context).size.height/36.65)
                         ),
                       )
                   ),
                   Positioned(
-                      left: 20,
-                      height: 250,
-                      width: 270,
+                      left: MediaQuery.of(context).size.width/18,
+                      height: MediaQuery.of(context).size.height/2.9,
+                      width: MediaQuery.of(context).size.height/1.3,
                       child: Container(
                         decoration: BoxDecoration(
                             color: Colors.grey,
-                            borderRadius: BorderRadius.circular(20)
+                            borderRadius: BorderRadius.circular(MediaQuery.of(context).size.height/36.65)
                         ),
                       )
                   )
@@ -74,42 +75,48 @@ class _CollectionsPageState extends State<CollectionsPage> {
               ),
             ),
           ),
-          const SizedBox(height: 10,),
+          SizedBox(height: MediaQuery.of(context).size.height/73,),
           Center(
-            child: AppLargeText(text: "Women", color: Colors.black, size: 30,),
+            child: AppLargeText(text: "Women", color: Colors.black, size: MediaQuery.of(context).size.width/12,),
           ),
-          const SizedBox(height: 15,),
+          SizedBox(height: MediaQuery.of(context).size.height/48.9,),
           Center(
             child: SizedBox(
-              width: 250,
+              width: MediaQuery.of(context).size.width/1.4,
               child: AppText(text: "Believe In Yourself, take on your challenges, dig within yourself to conquer Your fears",),
             ),
           ),
-          const SizedBox(height: 10,),
+          SizedBox(height: MediaQuery.of(context).size.height/73,),
           Row(
             children: [
               Container(
-                padding: const EdgeInsets.only(left: 10),
-                child: ResponsiveCircle(width: 10, height: 10,),
+                padding: EdgeInsets.only(left: MediaQuery.of(context).size.width/36),
+                child: Container(
+                  height: MediaQuery.of(context).size.height/73,
+                  width: MediaQuery.of(context).size.width/36,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle
+                  ),
+                ),
               ),
               Expanded(child: Container()),
               Container(
-                margin: const EdgeInsets.only(right: 30),
+                margin: EdgeInsets.only(right: MediaQuery.of(context).size.width/12),
                 child: Row(
                     children:[
                       Container(
-                        height: 50,
-                        width: 50,
+                        height: MediaQuery.of(context).size.height/14.6,
+                        width: MediaQuery.of(context).size.width/7.2,
                         decoration: const BoxDecoration(
                             color: Colors.grey,
                             shape: BoxShape.circle
                         ),
                         child: const Icon(Icons.arrow_back, color: Colors.white,),
                       ),
-                      const SizedBox(width: 10,),
+                      SizedBox(width: MediaQuery.of(context).size.width/36,),
                       Container(
-                        height: 50,
-                        width: 50,
+                        height: MediaQuery.of(context).size.height/14.6,
+                        width: MediaQuery.of(context).size.width/7.2,
                         decoration: const BoxDecoration(
                           color: Colors.red,
                           shape: BoxShape.circle
